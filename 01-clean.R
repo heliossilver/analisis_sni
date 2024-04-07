acaaac87be784a52843047697b5abfe69b1a19aa
Helios Gallego <heliossilver@gmail.com>
2024-04-07 12:05:21 -0700

    resumir perdida SNI

--- 01-clean.R
+++ 01-clean.R
@@ -16,4 +16,20 @@ if (!dir.exists("results")) {
   dir.create("results")
 }
 
+#guardar
 write.csv(x = data_filtrada, file = "results/limpia.csv", row.names = FALSE)
+
+#calcular la proporcion de DR y DRA que pierden SNI
+
+data_resumen <- data_filtrada %>% 
+  filter(cambio == "perdio SNI") %>% 
+  group_by(nobilis) %>% 
+  summarise(totales = sum(miembros))
+
+#guardar
+write.csv(x = data_resumen, file = "results/resumen.csv", row.names = FALSE)
+
+
+#TO-DOs
+#Calcular el % de DR. y DRA. que pierde SNI
+
